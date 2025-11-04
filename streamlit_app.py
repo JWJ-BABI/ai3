d5f96080d55367e4ccb845cff0e50b778479ffcb
JWJ-BABI <H098S02899@chunchon.gwe.hs.kr>
2025-11-04 09:51:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,6 +74,10 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
     # },
+
+    labels[0] : {"texts" : ["데리버거는 맛잇어"]},
+    labels[1] : {"texts" : ["라이스버거는 맛잇어"]},
+    labels[2] : {"texts" : ["새우버거는 맛잇어"]},
 }
 
 # ======================
