f071f40716b202d9c3e21f2bd8bdc6a7a2765270
JWJ-BABI <H098S02899@chunchon.gwe.hs.kr>
2025-11-14 15:24:43 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ st.markdown("---")
 CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
 
      labels[0]: {
-       "texts": ["짬뽕의 특징과 유래", "국물 맛 포인트", "지역별 스타일 차이"],
+       "texts": ["네이마르는 브라질의 축구선수입니다.", "네이마르는 여친이 많습니다.", "네이마르는 에버랜드에 왔었어요"],
        "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
        "videos": ["https://youtu.be/XXXXXXXXXXX"]
      },
